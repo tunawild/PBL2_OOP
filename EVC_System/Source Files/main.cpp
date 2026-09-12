2633b037d0c697ba87227f14aa1f03c54e633c6d
tunawild <tuanphong10032007ok@gmail.com>
2026-09-12 09:32:29 +0700

    thêm thư viện giao diện, các file phụ

--- EVC_System/Source Files/main.cpp
+++ EVC_System/Source Files/main.cpp
@@ -1,8 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include "raylib.h"
 
-int main(){
-    
-    
+int main() {
+    // Khởi tạo cửa sổ 800x450 với tiêu đề test
+    InitWindow(800, 450, "Test Raylib - PBL2 EV Station");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Raylib dang hoat dong tot!", 190, 200, 30, LIGHTGRAY);
+            DrawCircle(400, 300, 40, GREEN); // Vẽ 1 hình tròn màu xanh
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
